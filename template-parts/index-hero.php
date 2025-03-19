4f151c5ad0c66d1b99b7e262cf18ff8d69f34a59
Mialmadsen <miamad01@easv365.dk>
2025-03-19 09:46:43 +0100

    Loop update

--- template-parts/index-hero.php
+++ template-parts/index-hero.php
@@ -1,9 +1,17 @@
+<?php
+$heroTextSmallHeader1 = get_field("small_header_1");
+$heroTextHeader = get_field("big_header");
+$heroTextSmallHeader2 = get_field("small_header_2");
+$
+
+?>
+
 <section class="hero">
       <div class="hero-container">
         <div class="hero-text">
-          <p class="hero-text-small-header">#1 Bender's most shiny recipe</p>
-          <p class="hero-text-big-header">Boozy brown <span class="hero-text-bold">Lentil soup</span></p>
-          <p class="hero-text-small-header">40% healthy</p>
+          <p class="hero-text-small-header"><?php echo esc_html($heroTextSmallHeader1) ?></p>
+          <p class="hero-text-big-header"><?php echo esc_html($heroTextHeader) ?></p>
+          <p class="hero-text-small-header"><?php echo esc_html($heroTextSmallHeader2) ?></p>
         </div>
         <div class="hero-search">
           <input type="search" name="" class="search-box" placeholder="Search">
--- template-parts/index-todays.php
+++ template-parts/index-todays.php
@@ -3,7 +3,14 @@
         <h2>Today's recipes</h2>
       </div>
 
+      
       <div class="todays_recipes_content">
+        <?php
+        $arguments = array(
+          "post_type" => "todays-recipe-card",
+          "post_per_page" => 4
+        );
+        
         <div class="todays_recipes_content_block">
           <div class="todays_recipes_content_block_image">
             <img src="https://picsum.photos/300/175" alt="">
@@ -25,79 +32,6 @@
                   </button>
                 </div>
 
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="todays_recipes_content_block">
-          <div class="todays_recipes_content_block_image">
-            <img src="https://picsum.photos/300/175" alt="">
-          </div>
-          <div class="todays_recipes_content_block_text">
-            <div class="todays_recipes_content_block_text_top">
-              <h5>FoodName</h5>
-              <p>Lorem Ipsum is simply dummy text of the</p>
-            </div>
-            <div class="todays_recipes_content_block_text_bottom">
-              <div class="todays_recipes_content_block_bottom_area">
-                <div class="todays_recipes_content_block_bottom_area_left" data-category="chicken"> 
-                  <p class="category-color" data-category="chicken">Chicken</p>
-                  <p>Recipe</p>
-                </div>
-                <div class="todays_recipes_content_block_bottom_area_right">
-                  <button>
-                    <a href="#">How to</a>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="todays_recipes_content_block">
-          <div class="todays_recipes_content_block_image">
-            <img src="https://picsum.photos/300/175" alt="">
-          </div>
-          <div class="todays_recipes_content_block_text">
-            <div class="todays_recipes_content_block_text_top">
-              <h5>FoodName</h5>
-              <p>Lorem Ipsum is simply dummy text of the</p>
-            </div>
-            <div class="todays_recipes_content_block_text_bottom">
-              <div class="todays_recipes_content_block_bottom_area">
-                <div class="todays_recipes_content_block_bottom_area_left" data-category="pasta">
-                  <p id="cat-test" data-category="pasta"></p>
-                  <p>Recipe</p>
-                </div>
-                <div class="todays_recipes_content_block_bottom_area_right">
-                  <button>
-                    <a href="#">How to</a>
-                  </button>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="todays_recipes_content_block">
-          <div class="todays_recipes_content_block_image">
-            <img src="https://picsum.photos/300/175" alt="">
-          </div>
-          <div class="todays_recipes_content_block_text">
-            <div class="todays_recipes_content_block_text_top">
-              <h5>FoodName</h5>
-              <p>Lorem Ipsum is simply dummy text of the</p>
-            </div>
-            <div class="todays_recipes_content_block_text_bottom">
-              <div class="todays_recipes_content_block_bottom_area">
-                <div class="todays_recipes_content_block_bottom_area_left" data-category="meats">
-                  <p class="category-color category-meats" data-category="meats">Beef/Pork</p>
-                  <p>Recipe</p>
-                </div>
-                <div class="todays_recipes_content_block_bottom_area_right">
-                  <button>
-                    <a href="#">How to</a>
-                  </button>
-                </div>
               </div>
             </div>
           </div>
